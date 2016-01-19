dacf34eb451b2135919021a98bbd12351f429529
vtboyarc <adamrcarter@gmail.com>
2016-01-18 18:33:57 -0600

    Structs in Swift

--- ObjectOrientedSwift.playground/Contents.swift
+++ ObjectOrientedSwift.playground/Contents.swift
@@ -1,5 +1,18 @@
-//: Playground - noun: a place where people can play
+let x1 = 0
+let y1 = 0
 
-import UIKit
+// Specifying a constant as a touple with 2 ints, assgigned names to the values, x and y
+//Doing a scruct is easier than doing it this way, more resuable
+let coordinate1: (x: Int, y: Int) = (0,0)
 
-var str = "Hello, playground"
+// A Struct, that we named Point (Java class naming conventions basically)
+// This is a blueprint from which we can create many different Points
+struct Point {
+    // here we created 2 stored properties, with names x and y, of type Int
+    let x: Int
+    let y: Int
+}
+
+// Creates an Instance of the Struct "Point"
+let coordinatePoint = Point(x: 0, y: 0)
+coordinatePoint.x //shows the value of x
