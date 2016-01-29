c2ec8578602c8dc174b96057af0c7dc514b178fd
vtboyarc <adamrcarter@gmail.com>
2016-01-28 20:20:46 -0600

    optionals in swift

--- OptionalsInSwift.playground/Contents.swift
+++ OptionalsInSwift.playground/Contents.swift
@@ -26,3 +26,30 @@ if let newYorkAirport = airportCodes["JFK"] {
 } else {
     print("That key does not exist")
 }
+
+struct Friend {
+    let name: String
+    let age: String
+    let address: String? //an optional, ie, if no address exists
+}
+
+// Function that takes a dictionary with key of type String and value of type String, and returns from the Friend struct
+// returns an optional Friend, sincew chance we might get nil back if key/value pair doesn't exist
+func createFriend(dict: [String : String]) -> Friend? {
+    //if name and age don't exist and we get nil back, then exit the function scope and return nil
+    // guard lets you put the else in the same code block before using any braces
+    // using guard allows for less code, don't have to do nested if/else
+    guard let name = dict["name"], let age = dict["age"] else {
+        return nil
+    }
+    
+    let address = dict["address"]
+    
+    return Friend(name: name, age: age, address: address)
+    
+}
+
+
+
+
+
